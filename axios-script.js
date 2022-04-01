cbb9e5f2623672e29371b37c6cdc5fdc96ccce34
isaac_lau <42047110+pileofscraps@users.noreply.github.com>
2022-04-01 07:19:24 -0700

    Create axios-script.js

---
+++ axios-script.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+  let data = JSON.stringify({
+  "jsonrpc": "2.0",
+  "id": 0,
+  "method": "alchemy_getAssetTransfers",
+  "params": [
+    {
+      "fromBlock": "0x0",
+      "fromAddress": "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1",
+    }
+  ]
+});
+
+
+  var requestOptions = {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    data: data,
+  };
+
+  const apiKey = "demo"
+  const baseURL = 'https://eth-mainnet.alchemyapi.io/v2/${apiKey}';
+  const axiosURL = ${baseURL};
+
+  axios(axiosURL, requestOptions)
