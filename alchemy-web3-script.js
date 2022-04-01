d5caa0fd990d51e4284d278f616f110ecfeebb9e
isaac_lau <42047110+pileofscraps@users.noreply.github.com>
2022-04-01 07:18:21 -0700

    Create alchemy-web3-script.js

---
+++ alchemy-web3-script.js
@@ -0,0 +1,18 @@
+// alchemy-nft-api/alchemy-web3-script.js
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+
+// Replace with your Alchemy api key:
+const apiKey = "demo";
+
+// Initialize an alchemy-web3 instance:
+const web3 = createAlchemyWeb3(
+  https://eth-mainnet.alchemyapi.io/v2/${apiKey},
+);
+
+const data = await web3.alchemy.getAssetTransfers({
+  fromBlock: "0x0",
+  fromAddress: "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1",
+})
+
+// Print response:
+console.log(data);
