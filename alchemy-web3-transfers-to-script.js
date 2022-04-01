73bbed4e9ace11c6be0c2801338b8dc41746198a
isaac_lau <42047110+pileofscraps@users.noreply.github.com>
2022-04-01 14:14:44 -0700

    Create alchemy-web3-transfers-to-script.js

---
+++ alchemy-web3-transfers-to-script.js
@@ -0,0 +1,17 @@
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+
+// Replace with your Alchemy api key:
+const apiKey = "demo";
+
+// Initialize an alchemy-web3 instance:
+const web3 = createAlchemyWeb3(
+  https://eth-mainnet.alchemyapi.io/v2/${apiKey},
+);
+
+const data = await web3.alchemy.getAssetTransfers({
+  fromBlock: "0x0",
+  toAddress: "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1",
+})
+
+// Print response:
+console.log(data);
