f27036431deda0ace3373af1820b4cd1b15949cd
Aniket Gupta <aniketguptaknp@gmail.com>
2022-07-27 16:08:52 +0530

    Update nft-mints-alchemyweb3.js
    Update the script for the tutorial "get all NFTs minted by an address"

--- javascript/alchemyweb3/nft-mints/nft-mints-alchemyweb3.js
+++ javascript/alchemyweb3/nft-mints/nft-mints-alchemyweb3.js
@@ -1,32 +1,40 @@
-import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+// Setup: npm install alchemy-sdk
+import { Alchemy, Network } from "alchemy-sdk";
 
-// Replace with your Alchemy API key:
-const apiKey = "demo";
+const config = {
+  apiKey: "<-- ALCHEMY APP API KEY -->",
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
 
 // Address we want get NFT mints from
 const toAddress = "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1";
 
-// Initialize an alchemy-web3 instance:
-const web3 = createAlchemyWeb3(
-  `https://eth-mainnet.alchemyapi.io/v2/${apiKey}`,
-);
-
-const res = await web3.alchemy.getAssetTransfers({
+const res = await alchemy.core.getAssetTransfers({
   fromBlock: "0x0",
   fromAddress: "0x0000000000000000000000000000000000000000",
   toAddress: toAddress,
-  excludeZeroValue:true,
-  category: ["erc721","erc1155"]
-})
+  excludeZeroValue: true,
+  category: ["erc721", "erc1155"],
+});
 
 // Print contract address and tokenId for each NFT (ERC721 or ERC1155):
 for (const events of res.transfers) {
-    if (events.erc1155Metadata == null) {
-      console.log("ERC-721 Token Minted: ID- ", events.tokenId, " Contract- ", events.rawContract.address);
+  if (events.erc1155Metadata == null) {
+    console.log(
+      "ERC-721 Token Minted: ID- ",
+      events.tokenId,
+      " Contract- ",
+      events.rawContract.address
+    );
+  } else {
+    for (const erc1155 of events.erc1155Metadata) {
+      console.log(
+        "ERC-1155 Token Minted: ID- ",
+        erc1155.tokenId,
+        " Contract- ",
+        events.rawContract.address
+      );
     }
-    else{
-      for (const erc1155 of events.erc1155Metadata) {
-      console.log("ERC-1155 Token Minted: ID- ", erc1155.tokenId, " Contract- ", events.rawContract.address);
-      }
-    }
+  }
 }
