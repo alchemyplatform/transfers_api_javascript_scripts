cbc77482e9a741b6e7d5f2d9ce914f784c37cb33
Aniket Gupta <aniketguptaknp@gmail.com>
2022-07-27 16:21:54 +0530

    Use new Alchemy SDK

--- javascript/alchemyweb3/tx-history/tx-history-from-alchemyweb3.js
+++ javascript/alchemyweb3/tx-history/tx-history-from-alchemyweb3.js
@@ -1,17 +1,16 @@
-import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+// Setup: npm install alchemy-sdk
+import { Alchemy, Network } from "alchemy-sdk";
 
-// Replace with your Alchemy api key:
-const apiKey = "demo";
+const config = {
+  apiKey: "<-- ALCHEMY APP API KEY -->",
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
 
-// Initialize an alchemy-web3 instance:
-const web3 = createAlchemyWeb3(
-  `https://eth-mainnet.alchemyapi.io/v2/${apiKey}`,
-);
-
-const data = await web3.alchemy.getAssetTransfers({
+const data = await alchemy.core.getAssetTransfers({
   fromBlock: "0x0",
   fromAddress: "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1",
-})
+  category: ["external", "internal", "erc20", "erc721", "erc1155"],
+});
 
-// Print response:
 console.log(data);
