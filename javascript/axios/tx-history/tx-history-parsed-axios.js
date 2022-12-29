27849342806500b3fcbc8e1df7bdf1e5e82c5422
Dmitri Zaitsev <dmitri14@gmail.com>
2022-12-29 15:49:05 +0700

    Fix errors
    Running as it is produced errors: missing "category".
    This fix runs for me.

--- javascript/axios/tx-history/tx-history-parsed-axios.js
+++ javascript/axios/tx-history/tx-history-parsed-axios.js
@@ -8,10 +8,14 @@ import axios from 'axios';
     {
       "fromBlock": "0xA97AB8",
       "fromAddress": "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1",
+      "category": [
+          "external"
+      ],
     }
   ]
 });
 
+async function main () {
 
   var requestOptions = {
     method: 'post',
@@ -29,3 +33,7 @@ import axios from 'axios';
   for (const events of res.data.result.transfers) {
     console.log("Token Transfer: ", events.value, " ", events.asset);
   }
+  
+} 
+
+main()
